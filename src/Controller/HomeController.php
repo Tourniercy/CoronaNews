246b5306c0603ddca4c0a5de033abeaffe692870
Tourniercy <59143360+Tourniercy@users.noreply.github.com>
2020-04-20 12:16:18 +0200

    style

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpClient\HttpClient;
+
 
 class HomeController extends AbstractController
 {
@@ -12,8 +14,21 @@ class HomeController extends AbstractController
      */
     public function index()
     {
+        $client = HttpClient::create();
+        $response = $client->request('GET', 'http://newsapi.org/v2/top-headlines?country=fr&category=health&q=covid&apiKey=154a3122f10644b5ada441ea0aa94fe3');
+
+        $statusCode = $response->getStatusCode();
+        // $statusCode = 200
+        $contentType = $response->getHeaders()['content-type'][0];
+        // $contentType = 'application/json'
+        $content = $response->getContent();
+        // $content = '{"id":521583, "name":"symfony-docs", ...}'
+        $content = $response->toArray();
+        // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'articles' => $content,
+
         ]);
     }
 }
