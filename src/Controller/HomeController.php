bdf80306211a63fa514fed3341a07229f23d57a4
Ervin <ervin@github.com>
2020-04-20 17:20:25 +0200

    first

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpClient\HttpClient;
+use Knp\Component\Pager\PaginatorInterface;
 
 
 class HomeController extends AbstractController
@@ -14,10 +15,8 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="home")
      */
-    public function index(Request $request)
+    public function index(Request $request, PaginatorInterface $paginator)
     {
-
-        $country = $request->request->get('country');
 
         $client = HttpClient::create();
 
@@ -30,24 +29,23 @@ class HomeController extends AbstractController
 
         $defaultCountry = array_key_first($decodedData);
 
-        if ($country !== null) {
+        foreach($decodedData[$defaultCountry] as $value){
 
-          foreach($decodedData[$country] as $value){
-
-            $infected = $value['confirmed'];
-            $recovered = $value['recovered'];
-            $deaths = $value['deaths'];
-          }
-
-        } else {
-
-          foreach($decodedData[$defaultCountry] as $value){
-
-            $infected = $value['confirmed'];
-            $recovered = $value['recovered'];
-            $deaths = $value['deaths'];
-          }
+          $infected = $value['confirmed'];
+          $recovered = $value['recovered'];
+          $deaths = $value['deaths'];
         }
+
+        $array = [];
+
+        foreach($decodedData as $key => $value){
+
+          $lastData = end($value);
+          array_push($lastData, $key);
+          $array[$key] = $lastData;
+        }
+
+        dump($array);
 
         $fatality = ($deaths / $infected) * 100;
 
@@ -57,8 +55,14 @@ class HomeController extends AbstractController
 
         $content = $response->toArray();
 
+        $rows = $paginator->paginate(
+          $array,
+          $request->query->getInt('page', 1),
+          6
+        );
+
         return $this->render('home/index.html.twig', [
-          'data' => $decodedData,
+          'data' => $rows,
           'infected' => $infected,
           'recovered' => $recovered,
           'deaths' => $fatality,
@@ -66,28 +70,29 @@ class HomeController extends AbstractController
           'articles' => $content,
         ]);
     }
+
     /**
     * @Route("/changeCountry", name="changeCountry")
     */
     public function changeCountry(Request $request)
     {
 
-            $country = $request->request->get('country');
+        $country = $request->request->get('country');
 
-            $client = HttpClient::create();
+        $client = HttpClient::create();
 
-            $response = $client->request('GET', 'https://pomber.github.io/covid19/timeseries.json');
-            $data = $response->getContent();
+        $response = $client->request('GET', 'https://pomber.github.io/covid19/timeseries.json');
+        $data = $response->getContent();
 
-            $decodedData = json_decode($data, true);
+        $decodedData = json_decode($data, true);
 
-              foreach($decodedData[$country] as $value){
-                $infected = $value['confirmed'];
-                $recovered = $value['recovered'];
-                $deaths = $value['deaths'];
-              }
+          foreach($decodedData[$country] as $value){
+            $infected = $value['confirmed'];
+            $recovered = $value['recovered'];
+            $deaths = $value['deaths'];
+          }
 
-            $fatality = ($deaths / $infected) * 100;
-            return $this->json(['infected' => $infected,'recovered' => $recovered,'deaths' => $deaths,'fatality' => round($fatality,2)]);
+        $fatality = ($deaths / $infected) * 100;
+        return $this->json(['infected' => $infected,'recovered' => $recovered,'deaths' => $deaths,'fatality' => round($fatality,2)]);
     }
 }
